ae7bbd63abbde8942cdefcb2f7c407d37d762df0
zecong.nian <zecong.nian@aelf.io>
2023-10-13 14:43:16 +0800

    feat: graphQL mock

--- test/ProjectCopyServer.Application.Tests/Mocks/ProjectCopyServerApplicationTestBase_MockGraphQL.cs
+++ test/ProjectCopyServer.Application.Tests/Mocks/ProjectCopyServerApplicationTestBase_MockGraphQL.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GraphQL;
 using GraphQL.Client.Abstractions;
 using Moq;
 
@@ -12,7 +16,40 @@ public partial class ProjectCopyServerApplicationTestBase
     {
         return _mockGraphQlClient.Object;
     }
-    
-    
-    
+
+
+    protected void MockGraphQlRes<TRes>(TRes returns, string keyword, Dictionary<string, object>? expectedVariables = null)
+    {
+        var response = new GraphQLResponse<TRes>
+        {
+            Data = returns,
+        };
+        _mockGraphQlClient
+            .Setup(o => o.SendQueryAsync<TRes>(It.Is<GraphQLRequest>(req => 
+                    req.Query.Contains(keyword) &&
+                    AreVariablesMatching(req, expectedVariables)), 
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+    }
+
+    private bool AreVariablesMatching(GraphQLRequest request, IDictionary<string, object>? expectedVariables)
+    {
+        if (expectedVariables == null) return true;
+        foreach (var kvp in expectedVariables)
+        {
+            var actualValue = GetVariableValue(request.Variables, kvp.Key);
+            if (!object.Equals(actualValue, kvp.Value))
+                return false;
+        }
+
+        return true;
+    }
+
+    private object GetVariableValue(object variablesObj, string variableName)
+    {
+        var propertyInfo = variablesObj.GetType().GetProperty(variableName);
+        return propertyInfo?.GetValue(variablesObj);
+    }
+
+
 }
---
+++ test/ProjectCopyServer.Application.Tests/Samples/SampleTest_GraphQl.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProjectCopyServer.Samples.GraphQL;
+using Shouldly;
+using Xunit;
+
+namespace ProjectCopyServer.Samples;
+
+public partial class SampleTest
+{
+
+    [Fact]
+    public async Task GraphQlTest()
+    {
+        
+        MockGraphQlRes(new IndexerSymbols
+        {
+            TokenInfo = new List<SymbolInfo>{ new (){ Symbol = "USDT", Decimals = 2}}
+        }, "tokenInfo");
+        
+        var res = await _sampleService.GetTokenInfoAsync("USDT");
+        res.ShouldNotBeNull();
+        res.TokenInfo.ShouldNotBeEmpty();
+        res.TokenInfo[0].Symbol.ShouldBe("USDT");
+        res.TokenInfo[0].Decimals.ShouldBe(2);
+        
+        
+        MockGraphQlRes(new IndexerSymbols
+        {
+            TokenInfo = new List<SymbolInfo>{ new (){ Symbol = "USDT", Decimals = 3}}
+        }, "tokenInfo");
+        res = await _sampleService.GetTokenInfoAsync("USDT");
+        res.ShouldNotBeNull();
+        res.TokenInfo.ShouldNotBeEmpty();
+        res.TokenInfo[0].Symbol.ShouldBe("USDT");
+        res.TokenInfo[0].Decimals.ShouldBe(3);
+    }
+    
+    
+}
